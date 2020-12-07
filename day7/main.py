7190fbe54b3657d4141d04c8bb72f1ea4eb57e06
Clémence Bic <clemence.bic@hec.edu>
2020-12-07 15:51:39 +0100

    use dict instead of df

--- day7/main.py
+++ day7/main.py
@@ -4,37 +4,32 @@ import re
 f = open('day7/input.txt', 'r')
 input = f.read().split('\n')[:-1]
 
-idx = [i.split(' bags')[0] for i in input]
-contain = [i.split('bags contain ')[1].replace('.', '').split(', ') for i in input]
-contain_df = [pd.DataFrame({re.search('\d (.*) bag', j).group(1): int(j.split(' ')[0]) for j in i if bool(re.match('^\d', j))},
-                           index=[idx[index]]) for index, i in enumerate(contain)]
-
-df = pd.concat(contain_df)
+contain_dict = {i.split(' bags')[0]: {re.search('\d (.*) bag', j).group(1): int(j.split(' ')[0]) for j in i.split('bags contain ')[1].replace('.', '').split(', ') if bool(re.match('^\d', j))} for i in input}
 
 # Answer to Q1
-def get_contains_bag(color, df, bags_containers):
-    bags = list(df[df[color] >= 1].index)
+def get_contains_bag(color, contain_dict, bags_containers):
+    bags = [key for key, value in contain_dict.items() if color in value.keys()]
     new_bags = [i for i in bags if i not in bags_containers]
     bags_containers = bags_containers + new_bags
     if len(new_bags) > 0:
         for bag in new_bags:
-            if bag in list(df.columns):
-                bags_containers = get_contains_bag(bag, df, bags_containers)
+            if bag in list(contain_dict.keys()):
+                bags_containers = get_contains_bag(bag, contain_dict, bags_containers)
     return bags_containers
 
-
-print(f"Bag colors that can contain at least one shiny gold : {len(get_contains_bag('shiny gold', df, []))}")
+print(f"Bag colors that can contain at least one shiny gold : {len(get_contains_bag('shiny gold', contain_dict, []))}")
 
 
 # Answer to Q2
-def get_number_of_bags_to_contain(color, df):
+def get_number_of_bags_to_contain(color, contain_dict):
     count = 0
-    bags = list(df.columns[pd.notna(df.loc[color])])
+    #bags = list(df.columns[pd.notna(df.loc[color])])
+    bags = list(contain_dict[color].keys())
     for bag in bags:
-        if bag in df.index:
-            count += df.loc[color, bag]*(1 + get_number_of_bags_to_contain(bag, df))
+        if bag in contain_dict.keys():
+            count += contain_dict[color][bag]*(1 + get_number_of_bags_to_contain(bag, contain_dict))
         else:
-            count += df.loc[color, bag]
+            count += contain_dict[color][bag]
     return count
 
-print(f"A shiny gold gold should contain {get_number_of_bags_to_contain('shiny gold', df)} bags")
+print(f"A shiny gold gold should contain {get_number_of_bags_to_contain('shiny gold', contain_dict)} bags")
